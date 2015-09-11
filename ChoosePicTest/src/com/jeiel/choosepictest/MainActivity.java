2155e5f3a66e12e4baf885b7ffdd194dc4113ff1
Jeiel <550207595@qq.com>
2015-09-12 06:51:19 +0800

    added AndroidManifest.xml, finished ChoosePicTest

--- ChoosePicTest/src/com/jeiel/choosepictest/MainActivity.java
+++ ChoosePicTest/src/com/jeiel/choosepictest/MainActivity.java
@@ -1,84 +1,128 @@
-public class MainActivity extends Activity{
-	private static final String TAKE_PHOTO = 0;
-	private static final String CROP_PHOTO = 1;
-	private Button takePhoto;
-	private Button chooseFromAlbum;
-	private ImageView picture;
-	private Uri imageUri;
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		takePhoto = (Button) findViewById(R.id.take_photo);
-		chooseFromAlbum = (Button) findViewById(R.id.choose_from_album);
-		picture = (Button) findViewById(R.id.picture);
+package com.jeiel.choosepictest;
 
-		takePhoto.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
-				try{
-					if(outputImage.exists()){
-						outputImage.delete()
-					}
-					outputImage.createNewFile();
-				}catch(IOException e){
-					e.printStackTrace();
-				}
-				imageUri = Uri.fromFile(outputImage);
-				Intent intent = new Intent("android.media.ation.IMAGE_CAPTURE");
-				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-				startActivityForResult(intent, TAKE_PHOTO);
-			}
-		});
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-		chooseFromAlbum.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
-				try{
-					if(outputImage.exists()){
-						outputImage.delete();
-					}
-					outputImage.createNewFile();
-				}
-				imageUri = Uri.fromFile(outputImage);
-				Intent intent = new Intent("android.intent.action.GET_CONTENT");
-				intent.setType("image/*");
-				intent.putExtra("crop", true);
-				intent.putExtra("scale", true);
-				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-				startActivityForResult(intent, CROP_PHOTO);
-			}
-		});
-	}
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){
-		switch(requestCode){
-		case TAKE_PHOTO:
-			if(resultCode == RESULT_OK){
-				Intent intent = new Intent("com.android.camera.action.CROP");
-				intent.setDataAndType(imageUri, "image/*");
-				intent.putExtra("scale", true);
-				intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-				startActivityForResult(intent, CROP_PHOTO);
-			}
-			break;
-		case CROP_PHOTO:
-			if(resultCode == RESULT_OK){
-				try{
-					Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().
-						openInputStrem(imageUri));
-					picture.setImageBitmap(bitmap);
-				}catch(FileNotFoundException e){
-					e.printStackTrace();
-				}
-				
-			}
-			break;
-		default:
-			break;
-		}
-	}
+public class MainActivity extends Activity {
+    private static final int TAKE_PHOTO = 0;
+    private static final int CROP_PHOTO = 1;
+    private Button takePhoto;
+    private Button chooseFromAlbum;
+    private ImageView picture;
+    private Uri imageUri;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        takePhoto = (Button) findViewById(R.id.take_photo);
+        chooseFromAlbum = (Button) findViewById(R.id.choose_from_album);
+        picture = (ImageView) findViewById(R.id.picture);
+
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+
+        chooseFromAlbum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(Environment.getExternalStorageDirectory(), "output_image.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+                imageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent("android.intent.action.GET_CONTENT");
+                intent.setType("image/*");
+                intent.putExtra("crop", true);
+                intent.putExtra("scale", true);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, CROP_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch(requestCode){
+            case TAKE_PHOTO:
+                if(resultCode == RESULT_OK){
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CROP_PHOTO);
+                }
+                break;
+            case CROP_PHOTO:
+                if(resultCode == RESULT_OK){
+                    try{
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().
+                                openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    }catch(FileNotFoundException e){
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
