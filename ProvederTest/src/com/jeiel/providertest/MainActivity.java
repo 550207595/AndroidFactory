388ed60fd1c8c9796bb55aa36f025b39f4a77754
Jeiel <550207595@qq.com>
2015-09-11 19:48:43 +0800

    add AndroidManifest.xml and update MainActivity.java

--- ProvederTest/src/com/jeiel/providertest/MainActivity.java
+++ ProvederTest/src/com/jeiel/providertest/MainActivity.java
@@ -1,69 +1,102 @@
-public class MainActivity extends Activity{
+package com.jeiel.providertest;
 
-	private String newId;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button addBtn = (Button)findViewById(R.id.add_btn);
-		Button queryBtn = (Button)findViewById(R.id.query_btn);
-		Button updateBtn = (Button)findViewById(R.id.update_btn);
-		Button deleteBtn = (Button)findViewById(R.id.delete_btn);
+public class MainActivity extends Activity {
+    private String newId = "";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addBtn = (Button)findViewById(R.id.add_btn);
+        Button queryBtn = (Button)findViewById(R.id.query_btn);
+        Button updateBtn = (Button)findViewById(R.id.update_btn);
+        Button deleteBtn = (Button)findViewById(R.id.delete_btn);
 
-		addBtn.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book");
-				ContentValues values = new ContentValues();
-				values.put("name", "A Clash of Kings");
-				values.put("author", "George Martin");
-				values.put("pages", 1040);
-				values.put("price", 22.85);
-				Uri newUri = getContentResolver().insert(uri, values);
-				newId = newUri.getPathSegments().get(1);
-			}
-		});
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book");
+                ContentValues values = new ContentValues();
+                values.put("name", "A Clash of Kings");
+                values.put("author", "George Martin");
+                values.put("pages", 1040);
+                values.put("price", 22.85);
+                Uri newUri = getContentResolver().insert(uri, values);
+                newId = newUri.getPathSegments().get(1);
+            }
+        });
 
-		queryBtn.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book");
-				Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-				if(cursor != null){
-					while(cursor.moveToNext()){
-						String name = cursor.getString(cursor.getColumnIndex("name"));
-						String author = cursor.getString(cursor.getColumnIndex("author"));
-						int pages = cursor.getString(cursor.getColumnIndex("pages"));
-						double price = cursor.getDouble(cursor.getColumnIndex("price"));
-						Log.d("MainActivity", "book name is " + name);
-						Log.d("MainActivity", "book author is " + author);
-						Log.d("MainActivity", "book pages is " + pages);
-						Log.d("MainActivity", "book price is " + price);
-					}
-					cursor.close();
-				}
-			}
-		});
+        queryBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book");
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+                if(cursor != null){
+                    while(cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
 
-		updateBtn.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book/"+newId);
-				ContentValues values = new Contentvalues();
-				values.put("name", "A Storm of Swords");
-				values.put("pages", 1216);
-				values.put("price", 24.05);
-				getContentResolver().update(uri, values, null, null);
-			}
-		});
+        updateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book/" + newId);
+                ContentValues values = new ContentValues();
+                values.put("name", "A Storm of Swords");
+                values.put("pages", 1216);
+                values.put("price", 24.05);
+                getContentResolver().update(uri, values, null, null);
+            }
+        });
 
-		deleteBtn.setOnClickListener(new OnClickListener(){
-			@Override
-			public void onClick(View v){
-				Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book/"+newId);
-				getContentResolver().delete(uri, null, null);
-			}
-		});
-	}
+        deleteBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Uri uri = Uri.parse("content://com.jeiel.databasetest.provider/Book/" + newId);
+                getContentResolver().delete(uri, null, null);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
