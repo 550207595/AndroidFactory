89fc1ed396ece3d9df98985b596c1b5b79b1f8e6
Jeiel <550207595@qq.com>
2015-09-13 09:17:54 +0800

    added AndroidManifest.xml, finished ServiceBestPractice

--- ServiceBestPractice/src/com/jeiel/servicebestpractice/AlarmReceiver.java
+++ ServiceBestPractice/src/com/jeiel/servicebestpractice/AlarmReceiver.java
@@ -1,7 +1,13 @@
+package com.jeiel.servicebestpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 public class AlarmReceiver extends BroadcastReceiver{
 	@Override
 	public void onReceive(Context context, Intent intent){
-		Intent intent = new Intent(context, LongRunningService.class);
-		startService(intent);
+		Intent i = new Intent(context, LongRunningService.class);
+		context.startService(i);
 	}
 }
--- ServiceBestPractice/src/com/jeiel/servicebestpractice/LongRunningService.java
+++ ServiceBestPractice/src/com/jeiel/servicebestpractice/LongRunningService.java
@@ -1,3 +1,16 @@
+package com.jeiel.servicebestpractice;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.util.Date;
+
 public class LongRunningService extends Service{
 	@Override
 	public IBinder onBind(Intent intent){
@@ -5,18 +18,18 @@ public class LongRunningService extends Service{
 	}
 
 	@Override
-	public void onStartCommand(Intent intent, int flags, int startId){
+	public int onStartCommand(Intent intent, int flags, int startId){
 		new Thread(new Runnable(){
 			@Override
 			public void run(){
-				Log.d("LongRunningService", "executed at " + new Data().toString());
+				Log.d("LongRunningService", "executed at " + new Date().toString());
 			}
-		});
+		}).start();
 		AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		int aMinute = 60 * 1000;
-		long triggerAtTime = SystemClock.elapsedRealTime() + aMinute;
-		Intent intent = new Intent(this, AlarmReceiver.class);
-		PendingIntent pi = PendingIntent.getBroadcast(this, 0, intent ,0);
+		int tenSeconds = 10 * 1000;
+		long triggerAtTime = SystemClock.elapsedRealtime() + tenSeconds;
+		Intent i = new Intent(this, AlarmReceiver.class);
+		PendingIntent pi = PendingIntent.getBroadcast(this, 0, i ,0);
 		manager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
 		return super.onStartCommand(intent, flags, startId);
 	}
